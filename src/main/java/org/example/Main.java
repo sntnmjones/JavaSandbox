297ba5706d3e20a64dcb3e534fb8bb3fce1e5e36
Troy Jones <sntnmjones@gmail.com>
2023-12-24 06:18:34 -0800

    [CompletableFuture] add supplyAsync

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,16 @@ package org.example;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.example.threads.CompletableFutureSandbox;
 
 public class Main {
     private static final Logger LOGGER = LogManager.getLogger();
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        CompletableFutureSandbox completableFutureSandbox = new CompletableFutureSandbox();
+        completableFutureSandbox.asyncTest();
+    }
+
+    private void logTest() {
         LOGGER.trace("It is a trace logger.");
         LOGGER.debug("It is a debug logger.");
         LOGGER.info("It is a info logger.");
